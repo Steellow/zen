c7775de8546917963e965f8e474780ca42715fe5
Hannes Kinnunen <hankilapio@gmail.com>
2023-01-13 14:59:09 +0200

    Create basic in progress screen

--- lib/screens/in_progress_screen.dart
+++ lib/screens/in_progress_screen.dart
@@ -1,15 +1,30 @@
+import 'package:circular_countdown_timer/circular_countdown_timer.dart';
 import 'package:flutter/material.dart';
 import 'package:zen_app/util/styles.dart';
 
 class InProgressScreen extends StatelessWidget {
   const InProgressScreen({super.key});
 
+  final double circleSize = 150;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Styles.buttonColor,
       body: Center(
-        child: Text("Hello!"),
+        child: CircularCountDownTimer(
+          width: circleSize,
+          height: circleSize,
+          initialDuration: -3,
+          duration: 50,
+          ringColor: Styles.buttonColor,
+          fillColor: Colors.white,
+          strokeWidth: 10,
+          textStyle: const TextStyle(
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
     );
   }
