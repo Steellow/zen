c38f32520b54f57e8675f6cf2bbc9f40672db1af
Hannes Kinnunen <hankilapio@gmail.com>
2023-01-20 14:40:58 +0200

    Add functionality to TimesMeditatedTile

--- lib/settings_screen/tiles/times_meditated_tile.dart
+++ lib/settings_screen/tiles/times_meditated_tile.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:zen_app/common_widgets/center_icon.dart';
+import 'package:zen_app/util/hive_helper.dart';
 
 class TimesMeditadetTile extends StatelessWidget {
   const TimesMeditadetTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {},
-      leading: const CenterIcon(icon: Icon(Icons.remove_red_eye)),
-      title: const Text("1 395 sessions"),
-      subtitle: const Text("Times you've closed your eyes"),
+    return ValueListenableBuilder(
+      valueListenable: Hive.box(BOX_NAME_QUICK_STATS).listenable(),
+      builder: (context, box, child) {
+        return ListTile(
+          onTap: () {},
+          leading: const CenterIcon(icon: Icon(Icons.remove_red_eye)),
+          title: Text(
+            '${box.get(BOX_KEY_TOTAL_TIMES_MEDITATED, defaultValue: 0)} sessions',
+          ),
+          subtitle: const Text("Times you've closed your eyes"),
+        );
+      },
     );
   }
 }
