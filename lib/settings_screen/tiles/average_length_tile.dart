79516a7b5a5460502fea679b6e5f41f5a74ac70c
Hannes Kinnunen <hankilapio@gmail.com>
2023-01-21 12:12:18 +0200

    Add functionality to average meditation tile

--- lib/settings_screen/tiles/average_length_tile.dart
+++ lib/settings_screen/tiles/average_length_tile.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:zen_app/common_widgets/center_icon.dart';
+import 'package:zen_app/util/hive_helper.dart';
 
 class AverageLengthTile extends StatelessWidget {
   const AverageLengthTile({super.key});
+
+  String _getTitle() {
+    final Box box = Hive.box(BOX_NAME_QUICK_STATS);
+
+    final int secondMeditated =
+        box.get(BOX_KEY_TOTAL_SECONDS_MEDITATED, defaultValue: 0);
+    final int timesMeditated =
+        box.get(BOX_KEY_TOTAL_TIMES_MEDITATED, defaultValue: 0);
+
+    final double averageSeconds = secondMeditated / timesMeditated;
+    double averageMinutes = (averageSeconds / 60);
+
+    if (averageMinutes.isNaN) {
+      averageMinutes = 0.0;
+    }
+
+    return '${averageMinutes.toStringAsFixed(0)} minutes';
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       onTap: () {},
       leading: const CenterIcon(icon: Icon(Icons.alarm_on)),
-      title: const Text("12 minutes"),
+      title: Text(_getTitle()),
       subtitle: const Text("Your average meditation length"),
     );
   }
