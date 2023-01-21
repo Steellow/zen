66997953205420a8c7363789a99d1aba87572980
Hannes Kinnunen <hankilapio@gmail.com>
2023-01-21 12:18:58 +0200

    Make meditation time settings functional

--- lib/settings_screen/tiles/meditation_time_tile.dart
+++ lib/settings_screen/tiles/meditation_time_tile.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:zen_app/common_widgets/center_icon.dart';
+import 'package:zen_app/select_meditation_time_dialog/meditation_time_select_dialog.dart';
+import 'package:zen_app/util/hive_helper.dart';
 
 class MeditationTimeTile extends StatelessWidget {
   const MeditationTimeTile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {},
-      leading: const CenterIcon(icon: Icon(Icons.timer)),
-      title: const Text("Meditation time"),
-      subtitle: const Text("10 minutes - 20 minutes"),
+    return ValueListenableBuilder(
+      valueListenable: Hive.box(BOX_NAME_MEDITATION_TIME).listenable(),
+      builder: (context, box, widget) {
+        String minTime = box
+            .get(BOX_KEY_MIN_MEDITAION_TIME, defaultValue: 10.0)
+            .toStringAsFixed(0);
+        String maxTime = box
+            .get(BOX_KEY_MAX_MEDITAION_TIME, defaultValue: 20.0)
+            .toStringAsFixed(0);
+
+        return ListTile(
+          onTap: () => showMeditationTimeSelectDialog(context),
+          leading: const CenterIcon(icon: Icon(Icons.timer)),
+          title: const Text("Meditation time"),
+          subtitle: Text('$minTime minutes - $maxTime minutes'),
+        );
+      },
     );
   }
 }
